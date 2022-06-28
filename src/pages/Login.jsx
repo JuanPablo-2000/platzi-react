c8b85094067a35f7b577de6d9fbdb42725a8336e
JuanPablo-2000 <agudelo.14321@gmail.com>
2022-06-28 11:28:35 -0500

    useRef y formularios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import '@styles/Login.scss'
 
 import logo from '@logos/logo_yard_sale.svg';
 
 const Login = () => {
+    const form = useRef(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(form.current);
+        const data = {
+            username: formData.get('email'),
+            password: formData.get('password')
+        }
+        console.log(data);
+    }
+
     return (
         <>
         <div className="login">
             <div className="form-container">
                 <img src={logo} alt="logo" className="logo" />
 
-                <h1 className="title">Create a new password</h1>
-                <p className="subtitle">Enter a new passwrd for yue account</p>
-
-                <form action="/" className="form">
+                <form action="/" className="form" ref={form}>
                     <label htmlFor="password" className="label">Password</label>
-                    <input type="password" id="password" placeholder="*********" className="input input-password" />
+                    <input type="text" name="email" placeholder="platzi@example.com" className="input input-email" />
 
                     <label htmlFor="new-password" className="label">Password</label>
-                    <input type="password" id="new-password" placeholder="*********" className="input input-password" />
+                    <input type="password" name="password" placeholder="*********" className="input input-password" />
 
-                    <input type="submit" value="Confirm" className="primary-button login-button" />
+                    <button 
+                        className='primary-button login-button'
+                        onClick={handleSubmit}>
+                        Log in
+                    </button>
                 </form>
+                
+                <button 
+                    className="secondary-button signup-button">
+                    Sign up
+                </button>
             </div>
         </div>
         </>

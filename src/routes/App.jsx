da6b0b30f1fbe362a0627277fca225e5e6da4029
JuanPablo-2000 <agudelo.14321@gmail.com>
2022-06-27 20:22:39 -0500

    componentes y rutas de React Shop

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,17 @@
 import React, { Fragment } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
+import Login from '../pages/Login';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import CreateAccount from '../pages/CreateAccount';
+import MyAccount from '../pages/MyAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
+import PasswordRecovery from '../pages/PasswordRecovery';
 
 const App = () => {
     return (
@@ -14,10 +20,16 @@ const App = () => {
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/login' element={<Login />} />
+                    <Route path='/password-recovery' element={<PasswordRecovery />} />
                     
-                    <Route path='/recovery-password' element={<RecoveryPassword />} />
+                    <Route path='/send-email' element={<SendEmail />} />
+                    <Route path='/new-password' element={<NewPassword />} />
+
+                    <Route path='/account' element={<MyAccount />} />
+                    <Route path='/signup' element={<CreateAccount />} />
+                    <Route path='/checkout' element={<Checkout />} />
+                    <Route path='/orders' element={<Orders />} />
                     <Route path='/*' element={<NotFound />} />
-                    
                 </Routes>
             </Layout>
         </BrowserRouter>

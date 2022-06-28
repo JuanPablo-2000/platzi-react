45410c638123a3e1b653271654bceceb45248897
JuanPablo-2000 <agudelo.14321@gmail.com>
2022-06-28 14:05:18 -0500

    React Context

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -12,27 +12,32 @@ import MyAccount from '../pages/MyAccount';
 import Checkout from '../pages/Checkout';
 import Orders from '../pages/Orders';
 import PasswordRecovery from '../pages/PasswordRecovery';
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
 
 const App = () => {
+    const initialState = useInitialState();
     return (
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/password-recovery' element={<PasswordRecovery />} />
-                    
-                    <Route path='/send-email' element={<SendEmail />} />
-                    <Route path='/new-password' element={<NewPassword />} />
+        <AppContext.Provider value={initialState}>
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/password-recovery' element={<PasswordRecovery />} />
+                        
+                        <Route path='/send-email' element={<SendEmail />} />
+                        <Route path='/new-password' element={<NewPassword />} />
 
-                    <Route path='/account' element={<MyAccount />} />
-                    <Route path='/signup' element={<CreateAccount />} />
-                    <Route path='/checkout' element={<Checkout />} />
-                    <Route path='/orders' element={<Orders />} />
-                    <Route path='/*' element={<NotFound />} />
-                </Routes>
-            </Layout>
-        </BrowserRouter>
+                        <Route path='/account' element={<MyAccount />} />
+                        <Route path='/signup' element={<CreateAccount />} />
+                        <Route path='/checkout' element={<Checkout />} />
+                        <Route path='/orders' element={<Orders />} />
+                        <Route path='/*' element={<NotFound />} />
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
+        </AppContext.Provider>
     );
 }
 
